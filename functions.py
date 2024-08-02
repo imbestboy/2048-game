8b7bf07410821f21075dbb317066fb803c8dc734
SaJaD (ImBestBoy) <Mohamadsajad2012@gmail.com>
2024-08-02 17:05:28 +0330

    feat : user can move numbers in game board

--- functions.py
+++ functions.py
@@ -37,6 +37,11 @@ def draw_game_board(game_board: list[list[int]], is_start: bool = False) -> None
         game_board {list[list[int]]} -- game board detail
     """
     print("\n" * 40)
+    print("HELP :")
+    print("up => w")
+    print("left => a")
+    print("down => s")
+    print("right => d")
     if is_start:
         start_y = random.randint(0, len(game_board) - 1)
         start_x = random.randint(0, len(game_board[0]) - 1)
@@ -45,3 +50,49 @@ def draw_game_board(game_board: list[list[int]], is_start: bool = False) -> None
         for x, cell in enumerate(row):
             print(cell, end="\t")
         print()
+
+
+def move_numbers(game_board: list[list[int]], direction: str) -> list[list[int]]:
+    for _ in range(len(game_board)):
+        if direction == "up":
+            for y, row in enumerate(game_board):
+                for x, cell in enumerate(row):
+                    if y != 0 and cell != 0:
+                        if game_board[y - 1][x] == 0:
+                            game_board[y][x] = 0
+                            game_board[y - 1][x] = cell
+                        elif game_board[y - 1][x] == cell:
+                            game_board[y][x] = 0
+                            game_board[y - 1][x] = cell * 2
+        elif direction == "down":
+            for y, row in enumerate(game_board):
+                for x, cell in enumerate(row):
+                    if y != len(game_board) - 1 and cell != 0:
+                        if game_board[y + 1][x] == 0:
+                            game_board[y][x] = 0
+                            game_board[y + 1][x] = cell
+                        elif game_board[y + 1][x] == cell:
+                            game_board[y][x] = 0
+                            game_board[y + 1][x] = cell * 2
+    for _ in range(len(game_board[0])):
+        if direction == "left":
+            for y, row in enumerate(game_board):
+                for x, cell in enumerate(row):
+                    if x != 0 and cell != 0:
+                        if game_board[y][x - 1] == 0:
+                            game_board[y][x] = 0
+                            game_board[y][x - 1] = cell
+                        elif game_board[y][x - 1] == cell:
+                            game_board[y][x] = 0
+                            game_board[y][x - 1] = cell * 2
+        elif direction == "right":
+            for y, row in enumerate(game_board):
+                for x, cell in enumerate(row):
+                    if x != len(game_board[0]) - 1 and cell != 0:
+                        if game_board[y][x + 1] == 0:
+                            game_board[y][x] = 0
+                            game_board[y][x + 1] = cell
+                        elif game_board[y][x + 1] == cell:
+                            game_board[y][x] = 0
+                            game_board[y][x + 1] = cell * 2
+    return game_board
